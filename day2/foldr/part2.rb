e45f6708de6b689b03d03999c83e1fc01dbfffbd
Kuro <danieldominguez05@gmail.com>
2017-12-02 06:49:07 +0100

    Weird ruby

--- day2/foldr/part2.rb
+++ day2/foldr/part2.rb
@@ -1,10 +1,8 @@
-checksum = 0
-while line = gets
-  checksum += line.chomp.split(' ')
-                .map(&:to_i)
-                .combination(2)
-                .select {|p| p[0] % p[1] == 0 or p[1] % p[0] == 0 }
-                .map {|p| if p[0] % p[1] == 0 then p else p.reverse end }
-                .flatten.reduce(&:/)
-end
-puts checksum
+a=[]
+puts loop{if l=gets then a<<l else break a end}
+       .map{|l|l.chomp.split(' ')
+              .map(&:to_i)
+              .combination(2)
+              .select{|p|p[0]%p[1]==0 or p[1]%p[0]==0}
+              .map{|p| if p[0]%p[1]==0 then p else p.reverse end}
+              .flatten.reduce(&:/)}.sum

25c7b44a9b2ff12f81280d67f7d18bc7296e0883
Sergio Conde Gomez <skgsergio@gmail.com>
2017-12-05 17:09:05 +0100

    Day 5 🐍 ⭐ ⭐

--- day5/skgsergio/maze.py
+++ day5/skgsergio/maze.py
@@ -3,19 +3,27 @@
 import sys
 
 
-def part_1(l):
+def solve(l, part_2=False):
     idx = 0
     steps = 0
 
     while 0 <= idx < len(l):
         n = l[idx]
 
-        l[idx] = n + 1
+        l[idx] = n - 1 if part_2 and n >= 3 else n + 1
 
         idx += n
         steps += 1
 
     return steps
+
+
+def part_1(l):
+    return solve(l)
+
+
+def part_2(jumps):
+    return solve(l, True)
 
 
 if __name__ == '__main__':
@@ -27,4 +35,5 @@ if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
         l = list(map(int, f.read().split()))
 
-    print(part_1(l))
+    print(part_1(l[:]))
+    print(part_2(l[:]))

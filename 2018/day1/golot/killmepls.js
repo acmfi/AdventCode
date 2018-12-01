f85103ad4c40dc53fced5ce49c9c58b815e1c0e4
GoLoT <golot@golot.es>
2018-12-01 15:45:56 +0100

    Me quiero morir

--- 2018/day1/golot/killmepls.js
+++ 2018/day1/golot/killmepls.js
@@ -1,1 +1,8 @@
+//1
 eval(document.body.firstElementChild.textContent.split("\n").reduce((a, x) => a+x))
+
+//2
+var f = new Set();
+var r = NaN;
+var sum = 0;
+while(isNaN(r)) document.body.firstElementChild.textContent.split("\n").forEach(function(x) { n = parseInt(x); if(isNaN(n) || !isNaN(r)) return; sum+=n; if(f.has(sum)) r = sum; else f.add(sum); })

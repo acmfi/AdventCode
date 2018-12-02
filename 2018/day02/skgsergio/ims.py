447c1252ea47e2eee26b0172e78648d85748857a
Sergio Conde Gomez <skgsergio@gmail.com>
2018-12-02 13:39:43 +0100

    [skgsergio] Day 2: Python ⭐⭐

---
+++ 2018/day02/skgsergio/ims.py
@@ -0,0 +1,32 @@
+import sys
+
+def solve(l):
+    tw = 0
+    th = 0
+
+    for w in l:
+        cs = set(w)
+        tw += any(w.count(c) == 2 for c in cs)
+        th += any(w.count(c) == 3 for c in cs)
+
+    part1 = tw * th
+
+    part2 = set(''.join([c1 for c1, c2 in zip(a, b) if c1 == c2]) for a in l[:-1] for b in l[1:] if sum([c1 != c2 for c1, c2 in zip(a, b)]) == 1)
+
+    return part1, part2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: {} <input>".format(sys.argv[0]),
+              file=sys.stderr)
+        exit(1)
+
+    i = None
+    with open(sys.argv[1], 'r') as f:
+        i = f.read().splitlines()
+    
+    part1, part2 = solve(i)
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")

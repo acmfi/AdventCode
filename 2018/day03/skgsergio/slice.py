621c898978be0a266a0a9c16a77538f4f12eb5a6
Sergio Conde Gomez <skgsergio@gmail.com>
2018-12-03 09:55:22 +0100

    [skgsergio] Day 3: Python ⭐⭐

--- 2018/day03/skgsergio/slice.py
+++ 2018/day03/skgsergio/slice.py
@@ -7,17 +7,24 @@ from collections import defaultdict
 
 def solve(d):
     m = defaultdict(list)
+    o = {}
 
     for r in re.finditer(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", d):
         c, l, t, w, h = map(int, r.groups())
+        o[c] = set()
         for i in range(l, l + w):
             for j in range(t, t + h):
+                if len(m[(i, j)]) > 0:
+                    for n in m[(i, j)]:
+                        o[n].add(c)
+                        o[c].add(n)
                 m[(i, j)].append(c)
 
     plus1 = [i for i in m if len(m[i]) > 1]
     part1 = len(plus1)
 
-    part2 = None
+    notov = [c for c in o if len(o[c]) == 0]
+    part2 = notov[0]
 
     return part1, part2
 

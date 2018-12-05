73f0e65ca38cfa4a087a7607b5c917608036f564
Sergio Conde Gomez <skgsergio@gmail.com>
2018-12-05 09:10:44 +0100

    [skgsergio] Day 5: Python ⭐⭐

---
+++ 2018/day05/skgsergio/reduction.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+
+
+def react(chain):
+    poly = []
+
+    for unit in chain:
+        if poly and abs(ord(unit) - ord(poly[-1])) == 32:
+            poly.pop()
+        else:
+            poly.append(unit)
+
+    return poly
+
+
+def solve(c):
+    part1 = len(react(c))
+
+    part2 = min([len(react(c.replace(u, '').replace(u.upper(), ''))) for u in set(c.lower())])
+
+    return part1, part2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: {} <input>".format(sys.argv[0]),
+              file=sys.stderr)
+        exit(1)
+
+    d = None
+    with open(sys.argv[1], 'r') as f:
+        d = f.read().strip()
+
+    part1, part2 = solve(d)
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")

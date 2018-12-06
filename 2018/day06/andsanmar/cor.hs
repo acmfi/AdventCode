e265849e114c43a22de0550d956c94f311417d40
Andrés Sánchez <andres.sanchez.marin@alumnos.upm.es>
2018-12-06 14:20:57 +0100

    More readable

--- 2018/day06/andsanmar/cor.hs
+++ 2018/day06/andsanmar/cor.hs
@@ -2,27 +2,26 @@ import Data.Map.Strict (toList, fromListWith)
 import Data.List (sortBy)
 
 star1 :: [(Int, Int)] -> Int
-star1 s = maximum $ map snd (frequency $ map (\y -> nearest y s)  [(a,b) | a <- [(down fst s)..(up snd s)], b <- [(down snd s)..(up snd s)]])
-  where up = get maximum
-        down = get minimum
+star1 s = maximum $ map snd $ frequency $ map (nearest s) (inbounds s)
 
 frequency :: (Ord a) => [a] -> [(a, Int)]
 frequency xs = toList (fromListWith (+) [(x, 1) | x <- xs])
 
-nearest :: (Int, Int) -> [(Int, Int)] -> Maybe (Int,Int)
-nearest x s = if 1 < length (filter (\q -> q == e) (e:elements)) then Nothing else Just (snd e)
+nearest :: [(Int, Int)] -> (Int, Int) -> Maybe (Int,Int)
+nearest s x = if 1 < length (filter (==e) (e:elements)) then Nothing else Just (snd e)
   where (e:elements) = sortBy (\(a,_) (b,_) -> compare a b) $ map (\y -> (manhattan x y, y)) s
 
 manhattan :: (Int, Int) -> (Int,Int) -> Int
 manhattan (x,y) (a,b) = (abs $ x-a) + (abs $ y-b)
 
 star2 :: [(Int, Int)] -> Int
-star2 s = foldl (\n y -> if sum (map (manhattan y) s) <10000 then n+1 else n) 0 [(a,b) | a <- [(down fst s)..(up snd s)], b <- [(down snd s)..(up snd s)]]
-  where up = get maximum
-        down = get minimum
+star2 s = foldr (\y -> if sum (map (manhattan y) s) <10000 then (+1) else id) 0 (inbounds s)
 
-get :: ([a] -> c) -> (b -> a) -> [b] -> c
-get a b s = a $ map b s
+inbounds :: [(Int,Int)] -> [(Int,Int)]
+inbounds s = [(a,b) | a <- [(down fst s)..(up snd s)], b <- [(down snd s)..(up snd s)]]
+  where get a b p = a $ map b p
+        up = get maximum
+        down = get minimum
 
 main :: IO()
 main = do

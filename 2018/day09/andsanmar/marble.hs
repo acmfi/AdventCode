c239298a811d79917a59da4dc9aa23d972fc48c3
Andrés Sánchez <andres.sanchez.marin@alumnos.upm.es>
2018-12-10 09:59:36 +0100

    Day 09

---
+++ 2018/day09/andsanmar/marble.hs
@@ -0,0 +1,21 @@
+import Data.Sequence as Seq (Seq((:<|)), fromList, (><), (<|), (|>), singleton, splitAt, lookup, length, (!?))
+import System.Environment (getArgs)
+
+star1 :: Int -> Int -> Int
+star1 p n = (\(_,_,x) -> maximum x) $ foldl insert ((singleton 0)|>1,1,replicate p 0) [2..n]
+
+insert :: (Seq Int, Int, [Int]) -> Int -> (Seq Int, Int, [Int])
+insert (marbles,toIns,c) n = if mod n 23 == 0
+  then ((\(x,_:<|ys) -> x >< ys) $ Seq.splitAt (toIns-7) marbles, toIns-7,
+        (\(x,y:ys) -> x ++ (y + n + u):ys) $ Prelude.splitAt (mod n (Prelude.length c)) c)
+  else ((\(x,y) -> x >< n<|y) $ Seq.splitAt ((toIns+2) `mod` (Seq.length marbles)) marbles, (toIns+2) `mod` (Seq.length marbles), c)
+  where Just u = marbles !? ((toIns-7) `mod` Seq.length marbles)
+
+main :: IO()
+main = do
+  args <- getArgs
+  let players = read $ args !! 0
+  let marbles = read $ args !! 1
+  putStrLn $ show $ star1 players marbles
+  putStrLn $ show $ star1 players (marbles*100)
+

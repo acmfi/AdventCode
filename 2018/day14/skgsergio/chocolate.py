6adebc12f2fa03a7ff863c8718e807c8663afb30
Sergio Conde Gomez <skgsergio@gmail.com>
2018-12-14 09:46:14 +0100

    [skgsergio] Day 14: Python ⭐⭐

--- 2018/day14/skgsergio/chocolate.py
+++ 2018/day14/skgsergio/chocolate.py
@@ -2,21 +2,27 @@
 import sys
 
 
-def solve(d):
+def solve(n):
+    ds = [int(d) for d in str(n)]
     sc = [3, 7]
     e1 = 0
     e2 = 1
 
-    while len(sc) < d + 10:
+    part1 = None
+    part2 = None
+
+    while not part1 or not part2:
         t = sc[e1] + sc[e2]
         sc.extend(divmod(t, 10) if t >= 10 else [t])
 
         e1 = (e1 + 1 + sc[e1]) % len(sc)
         e2 = (e2 + 1 + sc[e2]) % len(sc)
 
-    part1 = ''.join(map(str, sc[d:d+10]))
+        if len(sc) == n + 10:
+            part1 = ''.join(map(str, sc[n:n+10]))
 
-    part2 = None
+        if sc[-len(ds):] == ds or sc[-len(ds)-1:-1] == ds:
+            part2 = len(sc) - len(ds) - (sc[-len(ds)-1:-1] == ds)
 
     return part1, part2
 

b81143815b75a4746dc93b0c05f07b0577d9fdfd
Pepe <j.fagoagas@alumnos.upm.es>
2019-12-01 20:30:18 +0000

    P1 + P2

--- 2019/day01/jfagoagas/day1.go
+++ 2019/day01/jfagoagas/day1.go
@@ -8,35 +8,21 @@ import (
 )
 
 func main() {
-    // Part 1
-	input1 := os.Args[1]
-	file1, err := os.Open(input1)
-	if err != nil {
-		os.Exit(1)
-	}
-	defer file1.Close()
-
-	scanner1 := bufio.NewScanner(file1)
+	// Part 1
+	scanner := bufio.NewScanner(openInput())
 	var total, fuel int = 0, 0
-	for scanner1.Scan() {
-		mass, _ := strconv.Atoi(scanner1.Text())
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
 		fuel = calcFuel(mass)
 		total += fuel
 	}
 	fmt.Println("Part 1 - Total fuel:", total)
 
-    // Part 2
-	input2 := os.Args[1]
-	file2, err := os.Open(input2)
-	if err != nil {
-		os.Exit(1)
-	}
-	defer file2.Close()
-
-	scanner2 := bufio.NewScanner(file2)
+	// Part 2
+	scanner = bufio.NewScanner(openInput())
 	total, fuel = 0, 0
-	for scanner2.Scan() {
-		mass, _ := strconv.Atoi(scanner2.Text())
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
 		fuel = calcFuel(mass)
 		for fuel > 0 {
 			total += fuel
@@ -44,6 +30,15 @@ func main() {
 		}
 	}
 	fmt.Println("Part 2 - Total fuel:", total)
+}
+
+func openInput() (file *os.File) {
+	input := os.Args[1]
+	file, err := os.Open(input)
+	if err != nil {
+		os.Exit(1)
+	}
+	return
 }
 
 func calcFuel(mass int) (fuel int) {

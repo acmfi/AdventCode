3f5b843002aeef69e55dbfbe7255760d52c936c6
Andrés Sánchez <andres.sanchez@epfl.ch>
2019-12-02 10:40:44 +0100

    Cleanup

--- 2019/day01/andsanmar/main.hs
+++ 2019/day01/andsanmar/main.hs
@@ -1,18 +1,19 @@
 import System.Environment
 
 transf :: Integer -> Integer
-transf x = (div x 3) - 2
+transf x = div x 3 - 2
 
 requireFuel :: Integer -> [Integer]
-requireFuel x = if x <= 0 || newfuel <= 0 then [0] else newfuel:(requireFuel newfuel)
+requireFuel x = if x <= 0 || newfuel <= 0 then [0] else newfuel:requireFuel newfuel
   where newfuel = transf x
 
+main :: IO ()
 main = do
   (f:_) <- getArgs
   s  <- readFile f
-  let result = map (\x -> transf $ read x) (lines s)
+  let result = map (transf . read) (lines s)
   -- putStrLn $ show (result)
-  putStrLn $ show (sum result)
-  let result2 = map (\r -> requireFuel r) result
+  print (sum result)
+  let result2 = map requireFuel result
   -- putStrLn $ show result2
-  putStrLn $ show (sum result + sum (map sum result2))
+  print (sum result + sum (map sum result2))
--- 2019/day02/andsanmar/main.hs
+++ 2019/day02/andsanmar/main.hs
@@ -1,11 +1,7 @@
 import System.Environment
-import Control.Monad
 
 star1 :: [Int] -> Int -> Int -- List, Index
 star1 l index
-  | index >= length l = head l -- Limit
-  | l !! index /= 1 && l !! index /= 2 = head l -- Opcodes
-  | i1 >= length l || i2 >= length l || out >= length l = head l
   | op == 1 = star1 (newList (+)) newIndex
   | op == 2 = star1 (newList (*)) newIndex
   | otherwise = head l
@@ -19,21 +15,20 @@ star2 l (i1, i2)
   | sol > 19690720 = star2 l (i1-1, i2)
   | sol < 19690720 = star2 l (i1+1, i2+1)
   where sol = star1 (preproc l (i1, 1) (i2, 2)) 0
+star2 _ (_,_) = undefined
 
 replace :: [Int] -> Int -> Int -> [Int] -- List, Index, NewValue
-replace l n p = (\(l,(_:r)) -> l ++ n:r) $ splitAt p l
+replace s n p = (\(l,_:r) -> l ++ n:r) $ splitAt p s
 
 preproc :: [Int] -> (Int, Int) -> (Int, Int) -> [Int]
-preproc l (a1,a2) (b1,b2) = (replace (replace l a1 a2) b1 b2)
+preproc l (a1,a2) (b1,b2) = replace (replace l a1 a2) b1 b2
 
 main :: IO()
 main = do
   (f:_) <- getArgs
   text <-  readFile f
   let nums = read $ ('[':text) ++ "]"
-  putStrLn $ show nums
   let r1 = star1 (preproc nums (12,1) (2,2)) 0
-  putStrLn $ show r1
+  print r1
   let r2 = star2 nums (0,0)
-  putStrLn $ show r2
-  -- putStrLn $ show $ star2 ids
+  print r2

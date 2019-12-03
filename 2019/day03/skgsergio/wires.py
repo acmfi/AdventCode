66bed9c729223c29ff985ad32f83b59e394fb98a
Sergio Conde Gomez <skgsergio@gmail.com>
2019-12-03 07:41:33 +0100

    [skgsergio] Day 3: 🐍3️⃣ ⭐️⭐️

--- 2019/day03/skgsergio/wires.py
+++ 2019/day03/skgsergio/wires.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 import sys
 
-from typing import List, Set, Tuple
+from typing import List, Dict, Tuple
 
 
 M = {'L': (-1, 0), 'R': (1, 0), 'U': (0, 1), 'D': (0, -1)}
 
 
-def points(data: List[str]) -> Set[Tuple[int, int]]:
+def points(data: List[str]) -> Dict[Tuple[int, int], int]:
     x, y = 0, 0
-    p = set()
+    c = 0
+    p: Dict[Tuple[int, int], int] = {}
 
     for m in data:
         for _ in range(int(m[1:])):
             x += M[m[0]][0]
             y += M[m[0]][1]
+            c += 1
 
-            p.add((x, y))
+            if (x, y) not in p:
+                p[(x, y)] = c
 
     return p
 
 
-def part1(path1: List[str], path2: List[str]) -> int:
+def solve(path1: List[str], path2: List[str]) -> Tuple[int, int]:
     p1 = points(path1)
     p2 = points(path2)
-    p = set(p1) & set(p2)
+    ps = set(p1.keys()) & set(p2.keys())
 
-    return min([abs(x) + abs(y) for (x, y) in p])
+    dist = min([abs(x) + abs(y) for (x, y) in ps])
+    steps = min([p1[p] + p2[p] for p in ps])
+
+    return dist, steps
 
 
 if __name__ == '__main__':
@@ -41,4 +47,7 @@ if __name__ == '__main__':
     path1 = d[0].split(',')
     path2 = d[1].split(',')
 
-    print(f"Part 1: {part1(path1, path2)}")
+    part1, part2 = solve(path1, path2)
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")

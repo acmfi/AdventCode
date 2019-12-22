ff0c1bc9fc0e9ffa5ef20293e9c595ad65853aa3
Sergio Conde Gomez <skgsergio@gmail.com>
2019-12-22 13:36:53 +0100

    [skgsergio] Day 22: 🐍3️⃣ ⭐️⭐️

--- 2019/day22/skgsergio/shuffle.py
+++ 2019/day22/skgsergio/shuffle.py
@@ -48,7 +48,55 @@ def part1(data: List[Tuple[Action, int]]) -> int:
 
 
 def part2(data: List[Tuple[Action, int]]) -> int:
-    return 0
+    """
+    DISCLAMER:
+
+    My first attempt result was the following:
+
+    pypy3(29530,0x10a32ad40) malloc: can't allocate region
+    *** mach_vm_map(size=954525740113920) failed (error code=3)
+    pypy3(29530,0x10a32ad40) malloc: *** set a breakpoint in malloc_error_break to debug
+
+    My second attempt resulted in a code that was running (and never ended,
+    it ran for more than 50 minutes) while I looked for inspiration as I knew
+    this approach wasn't going to work.
+
+    The following approach is ~stolen~ inspired from the following AoC Reddit
+    solution thread comment, I did not had a look to the code, although it was
+    conveniently coded in Python, since the comment is enough to understand the
+    problem and the solution he came with.
+
+    https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnkaju/
+
+    All the credit goes to /r/mcpower_ (https://github.com/mcpower/)
+
+    I don't deserve the star :(
+    """
+    deck = 119315717514047
+    iterations = 101741582076661
+
+    imult = 1
+    odiff = 0
+
+    for t in data:
+        if t[0] == Action.DEAL_INTO:
+            imult = (imult * -1) % deck
+            odiff = (odiff + imult) % deck
+
+        elif t[0] == Action.CUT:
+            odiff = (odiff + (t[1] * imult)) % deck
+
+        elif t[0] == Action.DEAL_WITH:
+            imult = (imult * pow(t[1], deck - 2, deck)) % deck
+
+        else:
+            raise Exception(f"Unknown action: {t[0]}")
+
+
+    increment = pow(imult, iterations, deck)
+    offset = (odiff * (1 - increment) * pow((1 - imult) % deck, deck - 2, deck)) % deck
+
+    return (offset + 2020 * increment) % deck
 
 
 if __name__ == '__main__':

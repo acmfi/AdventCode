9aa07aab26b1187d3fd0bdffbd0c7a9e32c871d6
Charly98cma <1998cma@gmail.com>
2020-12-03 11:14:24 +0100

    Changed behavior to do second star

--- 2020/day03/Charly98cma/day03.py
+++ 2020/day03/Charly98cma/day03.py
@@ -1,11 +1,17 @@
-with open("input.txt", "r") as f:
-    f.readline() # Dump the first line
-    treeCounter = 0
-    row = 3
-    for l in f:
-        line = list(l.strip())
-        if line[row] == '#':
-            treeCounter += 1
-        row += 3
-        row %= len(line)
-    print("1st STAR SOLUTION =", treeCounter)
+def star1():
+    with open("input.txt", "r") as f:
+        length = len(f.readline().strip()) # Dump the first line
+        forest = []
+        treeCounter = 0
+        row = 0
+        col = 3
+        for l in f:
+            forest.append(list(l.strip()))
+            if forest[row][col] == '#':
+                treeCounter += 1
+            col = (col + 3) % length
+            row += 1
+        print("1st STAR SOLUTION =", treeCounter)
+
+
+star1()

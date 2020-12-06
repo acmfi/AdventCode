1c9c0159e8265bbaeaf725fe69dd6fc952b9d457
Charly98cma <1998cma@gmail.com>
2020-12-06 20:33:37 +0100

    Day 05: Python :snake::star::star: - Thanks to u/rune_kg ;)

--- 2020/day05/Charly98cma/day05.py
+++ 2020/day05/Charly98cma/day05.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python
 import sys
 
-with open(sys.argv[1], 'r') as myFile:
-    passes = []
-    maxID = 0
-    minID = sys.maxsize
-    for l in myFile:
-        ID = int(l.replace('F','0').replace('L','0').replace('B','1').replace('R','1'), 2)
-        passes.append(ID)
-        maxID = max(maxID, ID)
-        minID = min(minID, ID)
-    passes.sort()
-    print("1st STAR SOLUTION ->", maxID)
-    print("2nd STAR SOLUTION ->", set(list(range(minID,maxID))) - set(passes))
+passes = {int(l.translate(str.maketrans('FBLR', '0101')), 2) for l in open(sys.argv[1],'r')}
+print("1st STAR SOLUTION ->", max(passes))
+print("2nd STAR SOLUTION ->", set(range(min(passes), max(passes))) - passes)

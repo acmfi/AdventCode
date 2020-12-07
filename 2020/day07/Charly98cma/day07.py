b0d3db18a6da24c4c53a926a124b37faa4a669fd
Charly98cma <1998cma@gmail.com>
2020-12-07 13:12:54 +0100

    Day 07: Python :snake: :star: (more one-liners)

--- 2020/day07/Charly98cma/day07.py
+++ 2020/day07/Charly98cma/day07.py
@@ -3,8 +3,4 @@ import sys
 bags = ['shiny gold']
 rules = [rule.split(' contain ') for rule in open(sys.argv[1],'r').read().replace('.','').replace(' bags','').split('\n')[:-1]]
 
-for bag in bags:
-    for x in rules:
-        if (bag in x[1] and x[0] not in bags):
-            bags.append(x[0])
-print("1st STAR SOLUTION ->", len(bags)-1)
+print("1st STAR SOLUTION ->", len([bags.append(x[0]) for bag in bags for x in rules if (bag in x[1] and x[0] not in bags)]))

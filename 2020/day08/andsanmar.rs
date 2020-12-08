69dae8632fbea60d451afa40dff5ed76805bb21e
Andrés Sánchez <andres.sanchez@epfl.ch>
2020-12-08 11:20:05 +0100

    rusty day08 2020 **

---
+++ 2020/day08/andsanmar.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+#[derive(Debug)]
+enum Ins { Nop(i16), Acc(i16), Jmp(i16) }
+
+fn inf_loop(instructions : &Vec<Ins>, index : usize, acc : i32, executed : &mut Vec<usize>) -> (i32, bool) {
+    if index == instructions.len() { (acc, true) } else 
+        if executed.contains(&index) { (acc, false) } else {
+            executed.push(index);
+            match instructions[index] {
+                Ins::Nop(_) => inf_loop(instructions, index+1, acc, executed),
+                Ins::Jmp(n) => inf_loop(instructions, index+n as usize, acc, executed),
+                Ins::Acc(n) => inf_loop(instructions, index+1, acc+n as i32, executed)
+            }
+        }
+}
+
+fn solve_loop(instructions : &Vec<Ins>, index : usize, acc : i32, executed : &mut Vec<usize>) -> i32 {
+    match instructions[index] {
+        Ins::Nop(n) => {
+            let (r, end) = inf_loop(instructions, index+n as usize, acc, executed);
+            if end { r } else {solve_loop(instructions, index+1, acc, executed)}
+        },  
+        Ins::Jmp(n) => {
+            let (r, end) = inf_loop(instructions, index+1, acc, executed);
+            if end { r } else {solve_loop(instructions, index+n as usize, acc, executed)}
+        },
+        Ins::Acc(n) => solve_loop(instructions, index+1, acc+n as i32, executed),
+    }
+}
+
+fn main () {
+    let r = fs::read_to_string("input").unwrap();
+    let instructions : Vec<Ins> = r.lines().map(|l| {
+        let mut i = l.split_whitespace();
+        let ins = match i.next().unwrap() {
+            "nop" => Ins::Nop,
+            "acc" => Ins::Acc,
+            "jmp" => Ins::Jmp,
+            _ => panic!()
+        };
+        ins(i.next().unwrap().parse::<i16>().unwrap())
+    }).collect();
+    let mut executed : Vec<usize> = Vec::new();
+    let star1 = inf_loop(&instructions, 0, 0, &mut executed);
+    println!("{:?}", star1);
+    let star2 = solve_loop(&instructions, 0, 0, &mut executed);
+    println!("{:?}", star2);    
+}

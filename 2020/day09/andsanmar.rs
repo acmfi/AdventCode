49b9262a64988436d390aee446853173a3a4b5ac
Andrés Sánchez <andres.sanchez@epfl.ch>
2020-12-09 16:13:09 +0100

    more clever sol2 (thanks @rockneurotiko)

--- 2020/day09/andsanmar.rs
+++ 2020/day09/andsanmar.rs
@@ -4,22 +4,24 @@ fn is_sum_of(r : usize, prev : &[usize]) -> bool {
     prev.iter().filter(|x| prev.contains(&(r-*x)) && r != *x*2 ).count() > 0
 }
 
-fn find_summing(r : usize, nums : &Vec<usize>) {
+fn find_summing(r : usize, nums : &Vec<usize>) -> usize {
+    let mut v : Vec<usize> = Vec::new();
+    let mut adding = 0;
     for x in 0..nums.len() {
-        let mut adding = 0;
-        for y in x.. {
-            adding += nums[y];
-            if adding > r || r == nums[y] {
-                break
-            } else if adding == r {
-                let range = nums[x..y+1].to_vec();
-                let min = range.iter().min().unwrap();
-                let max = range.iter().max().unwrap();
-                println!("{} {} {}", min, max, min+max);
-            }
+        v.push(nums[x]);
+        adding += nums[x];
+        while adding > r {
+            adding -= v.remove(0);
+        }
+        if adding == r {
+            let min = v.iter().min().unwrap();
+            let max = v.iter().max().unwrap();
+            return min+max;
         }
     }
+    0
 }
+
 
 fn main () {
     let r = fs::read_to_string("input").unwrap();
@@ -27,9 +29,8 @@ fn main () {
     for x in 25..nums.len() {
         if ! is_sum_of(nums[x], &nums[x-25..x]) {
             println!("{:?}", nums[x]);
-            find_summing(nums[x], &nums);
+            println!("{}", find_summing(nums[x], &nums));
             break
         }
-    }
-    
+    }    
 }

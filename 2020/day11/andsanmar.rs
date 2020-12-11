2eef9b2bbf21a6f076037036ff4b50f47142f751
Andrés Sánchez <andres.sanchez@epfl.ch>
2020-12-11 12:08:26 +0100

    more idiomatic solution

--- 2020/day11/andsanmar.rs
+++ 2020/day11/andsanmar.rs
@@ -4,52 +4,35 @@ use std::fs;
 enum Seat { Floor, Occupied, Empty }
 
 fn seat_people_1<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
-    let mut v : Vec<Vec<&Seat>> = Vec::new();
     let get_seat = |(x, y) : (usize, usize)| match seats.get(x) { None => None, Some(s) => s.get(y) };
-    for x in 0..seats.len() {
-        let mut row : Vec<&Seat> = Vec::new();
-        for y in 0..seats[x].len() {
+    let v : Vec<Vec<&Seat>> = (0..seats.len()).map(|x| {
+        (0..seats[x].len()).map(|y| {
             let c = [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)].iter().filter(|s| get_seat(**s) == Some(&&Seat::Occupied)).count();
-            row.push( match seats[x][y] {
+            match seats[x][y] {
                 Seat::Empty if c == 0 => &Seat::Occupied,
                 Seat::Occupied if c >= 4 => &Seat::Empty,
                 s => s
-            });
-        }
-        v.push(row);
-    }
+            }
+        }).collect()
+    }).collect();
     if &v != seats { seat_people_1(&v) } else { v }
 }
 
 fn seat_people_2<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
-    let mut v : Vec<Vec<&Seat>> = Vec::new();
-    for x in 0..seats.len() {
-        let mut row : Vec<&Seat> = Vec::new();
-        for y in 0..seats[x].len() {
-            let mut adjacent : Vec<&Seat> = Vec::new();
-            for cx in (0..x).rev() { if seats[cx][y] != &Seat::Floor { adjacent.push(seats[cx][y]); break}}
-            for cx in (x+1)..(seats.len()) { if seats[cx][y] != &Seat::Floor { adjacent.push(seats[cx][y]); break}}
-            for cy in (0..y).rev() { if seats[x][cy] != &Seat::Floor { adjacent.push(seats[x][cy]); break}}
-            for cy in (y+1)..(seats[x].len()) { if seats[x][cy] != &Seat::Floor { adjacent.push(seats[x][cy]); break}}
-            for a in 1.. { if x+a >= seats.len() || y+a >= seats[x].len() {break;} else if
-                           seats[x+a][y+a] != &Seat::Floor { adjacent.push(seats[x+a][y+a]); break}}
-            for a in 1.. { if x-a >= seats.len() || y-a >= seats[x].len() {break;} else if
-                           seats[x-a][y-a] != &Seat::Floor { adjacent.push(seats[x-a][y-a]); break}}
-            for a in 1.. {
-                if x+a >= seats.len() || y-a >= seats[x].len() {break;} else if
-                    seats[x+a][y-a] != &Seat::Floor { adjacent.push(seats[x+a][y-a]); break}}
-            for a in 1.. {
-                if x-a >= seats.len() || y+a >= seats[x].len() {break;} else if
-                    seats[x-a][y+a] != &Seat::Floor { adjacent.push(seats[x-a][y+a]); break}}
-            let c = adjacent.iter().filter(|s| **s == &Seat::Occupied).count();
-            row.push( match seats[x][y] {
+    let get_seat = |(x, y) : (usize, usize)| match seats.get(x) { None => None, Some(s) => s.get(y) };
+    let v : Vec<Vec<&Seat>> = (0..seats.len()).map(|x| {
+        (0..seats[x].len()).map(|y| {
+            let c : usize = [|x,y,a|(x+a,y),|x,y,a|(x+a,y+a),|x,y,a|(x+a,y-a),|x,y,a|(x-a,y-a),
+                             |x,y,a|(x-a,y),|x,y,a|(x-a,y+a),|x,y,a|(x,y-a),|x,y,a|(x,y+a)].iter().fold(
+                0, |acc,fun| { for a in 1.. { match get_seat(fun(x,y,a)) {
+                    Some(&&Seat::Floor) => {}, Some(&&Seat::Occupied) => return acc + 1, _ => return acc}} acc });
+            match seats[x][y] {
                 Seat::Empty if c == 0 => &Seat::Occupied,
                 Seat::Occupied if c >= 5 => &Seat::Empty,
                 s => s
-            });
-        }
-        v.push(row);
-    }
+            }
+        }).collect()
+    }).collect();
     if &v != seats { seat_people_2(&v) } else { v }
 }
 

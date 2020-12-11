0b5475ae9c5035a3786e0fe36457cc3ef7061034
Andrés Sánchez <andres.sanchez@epfl.ch>
2020-12-11 11:09:06 +0100

    star1 improvement

--- 2020/day11/andsanmar.rs
+++ 2020/day11/andsanmar.rs
@@ -1,23 +1,15 @@
 use std::fs;
 
-#[derive(Debug, PartialEq)] // , Eq, Copy, Clone
+#[derive(PartialEq)]
 enum Seat { Floor, Occupied, Empty }
 
 fn seat_people_1<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
     let mut v : Vec<Vec<&Seat>> = Vec::new();
+    let get_seat = |(x, y) : (usize, usize)| match seats.get(x) { None => None, Some(s) => s.get(y) };
     for x in 0..seats.len() {
         let mut row : Vec<&Seat> = Vec::new();
-        for y in 0..seats[x].len() { // TODO maybe better to do a zip?
-            let mut adjacent : Vec<&Seat> = Vec::new();
-            if x > 0 { adjacent.push(seats[x-1][y]);
-            if y > 0 { adjacent.push(seats[x-1][y-1]) }
-            if y < seats[x].len()-1 { adjacent.push(seats[x-1][y+1]) }}
-            if x < seats.len()-1 { adjacent.push(seats[x+1][y]);
-            if y > 0 { adjacent.push(seats[x+1][y-1]) }
-            if y < seats[x].len()-1 { adjacent.push(seats[x+1][y+1]) } }
-            if y > 0 { adjacent.push(seats[x][y-1]) }
-            if y < seats[x].len()-1 { adjacent.push(seats[x][y+1]) }
-            let c = adjacent.iter().filter(|s| *s == &&Seat::Occupied).count();
+        for y in 0..seats[x].len() {
+            let c = [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)].iter().filter(|s| get_seat(**s) == Some(&&Seat::Occupied)).count();
             row.push( match seats[x][y] {
                 Seat::Empty if c == 0 => &Seat::Occupied,
                 Seat::Occupied if c >= 4 => &Seat::Empty,
@@ -26,9 +18,7 @@ fn seat_people_1<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
         }
         v.push(row);
     }
-    if &v != seats {
-        seat_people_1(&v)
-    } else { v }
+    if &v != seats { seat_people_1(&v) } else { v }
 }
 
 fn seat_people_2<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
@@ -41,19 +31,17 @@ fn seat_people_2<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
             for cx in (x+1)..(seats.len()) { if seats[cx][y] != &Seat::Floor { adjacent.push(seats[cx][y]); break}}
             for cy in (0..y).rev() { if seats[x][cy] != &Seat::Floor { adjacent.push(seats[x][cy]); break}}
             for cy in (y+1)..(seats[x].len()) { if seats[x][cy] != &Seat::Floor { adjacent.push(seats[x][cy]); break}}
-            for a in 1.. {
-                if x+a >= seats.len() || y+a >= seats[x].len() {break;} else if
-                    seats[x+a][y+a] != &Seat::Floor { adjacent.push(seats[x+a][y+a]); break}}
-            for a in 1.. {
-                if x-a >= seats.len() || y-a >= seats[x].len() {break;} else if
-                    seats[x-a][y-a] != &Seat::Floor { adjacent.push(seats[x-a][y-a]); break}}
+            for a in 1.. { if x+a >= seats.len() || y+a >= seats[x].len() {break;} else if
+                           seats[x+a][y+a] != &Seat::Floor { adjacent.push(seats[x+a][y+a]); break}}
+            for a in 1.. { if x-a >= seats.len() || y-a >= seats[x].len() {break;} else if
+                           seats[x-a][y-a] != &Seat::Floor { adjacent.push(seats[x-a][y-a]); break}}
             for a in 1.. {
                 if x+a >= seats.len() || y-a >= seats[x].len() {break;} else if
                     seats[x+a][y-a] != &Seat::Floor { adjacent.push(seats[x+a][y-a]); break}}
             for a in 1.. {
                 if x-a >= seats.len() || y+a >= seats[x].len() {break;} else if
                     seats[x-a][y+a] != &Seat::Floor { adjacent.push(seats[x-a][y+a]); break}}
-            let c = adjacent.iter().filter(|s| *s == &&Seat::Occupied).count();
+            let c = adjacent.iter().filter(|s| **s == &Seat::Occupied).count();
             row.push( match seats[x][y] {
                 Seat::Empty if c == 0 => &Seat::Occupied,
                 Seat::Occupied if c >= 5 => &Seat::Empty,
@@ -62,9 +50,7 @@ fn seat_people_2<'a>(seats : &Vec<Vec<&'a Seat>>) -> Vec<Vec<&'a Seat>> {
         }
         v.push(row);
     }
-    if &v != seats {
-        seat_people_2(&v)
-    } else { v }
+    if &v != seats { seat_people_2(&v) } else { v }
 }
 
 

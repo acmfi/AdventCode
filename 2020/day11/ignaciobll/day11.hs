09df24b2883f2dcc7f49f1baf54ad17994690f32
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2020-12-12 23:43:27 +0100

    haskell: day 11 :star: :star:

--- 2020/day11/ignaciobll/day11.hs
+++ 2020/day11/ignaciobll/day11.hs
@@ -1,13 +1,15 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-16.24 script --package matrix
 
-
 import           Data.Foldable                  ( toList )
 
 import           Data.Matrix                    ( Matrix )
 import qualified Data.Matrix                   as M
 
-import           Data.Maybe                     ( catMaybes )
+import           Data.Maybe                     ( catMaybes
+                                                , maybeToList
+                                                , listToMaybe
+                                                )
 
 (!?) :: Matrix a -> (Int, Int) -> Maybe a
 (!?) = flip $ uncurry M.safeGet
@@ -21,8 +23,9 @@ instance Show Seat where
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "input.txt"
+  input <- parseInput <$> getContents
   putStrLn $ "Star 1: " ++ (show $ star1 input)
+  putStrLn $ "Star 2: " ++ (show $ star2 input)
 
 star1 :: Matrix Seat -> Int
 star1 m = count (== Occupied) . last $ fixpoint step1 m
@@ -34,7 +37,7 @@ fixpoint f a = a : (fmap snd $ takeWhile (\(a, b) -> a /= b) $ zip s s')
   s' = tail s
 
 step1 :: Matrix Seat -> Matrix Seat
-step1 m = M.mapPos (\pos seat -> seatStep seat (neighbours pos)) m
+step1 m = M.mapPos (\pos seat -> seatStep 4 seat (neighbours pos)) m
   where neighbours = catMaybes . fmap (m !?) . neighbourPos1
 
 neighbourPos1 :: (Int, Int) -> [(Int, Int)]
@@ -44,10 +47,43 @@ star2 :: Matrix Seat -> Int
 star2 m = count (== Occupied) . last $ fixpoint step2 m
 
 step2 :: Matrix Seat -> Matrix Seat
-step2 m = m
+step2 m = M.mapPos (\pos seat -> seatStep 5 seat (neighbours pos)) m
+ where
+  neighbours = (>>= id) . fmap fstSeat . neighbourPos2 (M.ncols m) (M.nrows m)
+  fstSeat :: [(Int, Int)] -> [Seat]
+  fstSeat = -- Takes the first seat distinct of Floor in given collection of points. Empty by default
+    maybeToList . listToMaybe . dropWhile (== Floor) . catMaybes . fmap (m !?)
 
-neighbourPos2 :: (Int, Int) -> [(Int, Int)]
-neighbourPos2 = undefined
+neighbourPos2 :: Int -> Int -> (Int, Int) -> [[(Int, Int)]]
+neighbourPos2 cols rows (x, y) =
+  [ reverse [ (x, y') | y' <- [1 .. y - 1] ] -- Left
+  , id [ (x - x', y - y') | y' <- [1 .. y - 1], x' <- [1 .. x - 1], x' == y' ] -- Above Left
+  , reverse [ (x', y) | x' <- [1 .. x - 1] ] -- Above
+  , [ (x - x', y + y')
+    | x' <- [1 .. rows]
+    , y' <- [1 .. cols]
+    , x' == y'
+    , x - x' >= 1
+    , y + y' <= cols
+    ] -- Above Right
+  , id [ (x, y') | y' <- [y + 1 .. cols] ] -- Right
+  , [ (x' + x, y' + y)
+    | y' <- [1 .. rows]
+    , x' <- [1 .. cols]
+    , x' == y'
+    , x' + x <= rows
+    , y' + y <= cols
+    ] -- Below Right
+  , id [ (x', y) | x' <- [x + 1 .. rows] ] -- Below
+  , [ (x + x', y - y')
+    | x' <- [1 .. rows]
+    , y' <- [1 .. cols]
+    , x' == y'
+    , x + x' <= rows
+    , y - y' >= 1
+    ] -- Below Left
+  ]
+
 
 parseInput :: String -> Matrix Seat
 parseInput = M.fromLists . fmap (fmap parseSeat) . lines
@@ -57,12 +93,12 @@ parseSeat 'L' = Empty
 parseSeat '.' = Floor
 parseSeat '#' = Occupied
 
-seatStep :: Seat -> [Seat] -> Seat
-seatStep Floor _ = Floor
-seatStep Empty near | count (== Occupied) near == 0 = Occupied
-                    | otherwise                     = Empty
-seatStep Occupied near | count (== Occupied) near >= 4 = Empty
-                       | otherwise                     = Occupied
+seatStep :: Int -> Seat -> [Seat] -> Seat
+seatStep _ Floor _ = Floor
+seatStep _ Empty near | count (== Occupied) near == 0 = Occupied
+                      | otherwise                     = Empty
+seatStep n Occupied near | count (== Occupied) near >= n = Empty
+                         | otherwise                     = Occupied
 
 count :: Foldable f => (a -> Bool) -> f a -> Int
 count pred = length . filter pred . toList

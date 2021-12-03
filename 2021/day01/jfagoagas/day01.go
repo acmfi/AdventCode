123f52df93f12da0c7aa8b0c826cc0ca88fac6cb
Pepe Fagoaga <jose.fagoaga@smartprotection.com>
2021-12-03 10:18:20 +0100

    delete aux file

--- 2021/day01/jfagoagas/day01.go
+++ 2021/day01/jfagoagas/day01.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -34,4 +36,27 @@ func depthIncreases2(input []int) int {
 		depth = value
 	}
 	return depthIncreases
+}
+
+func readLines(path string) []int {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Can't open the input file for reading")
+		os.Exit(1)
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Can't close file")
+			os.Exit(1)
+		}
+	}()
+
+	var lines []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line, _ := strconv.Atoi(scanner.Text())
+		lines = append(lines, line)
+	}
+	return lines
 }

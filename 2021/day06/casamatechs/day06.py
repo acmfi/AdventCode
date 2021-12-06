ee0a4ce98a432ddc88815dc420184a6a9ac88898
Carlos Sanchez <casamatechs@gmail.com>
2021-12-06 17:52:14 +0900

    Star 1 and 2 printed

--- 2021/day06/casamatechs/day06.py
+++ 2021/day06/casamatechs/day06.py
@@ -10,7 +10,11 @@ fishes_spawn = np.zeros(shape=(9,1),dtype=np.int64)
 for fish in fishes:
     fishes_spawn[fish] += 1
 
-for i in range(256):
-    fishes_spawn = np.roll(fishes_spawn,-1)
-    fishes_spawn[6] += fishes_spawn[-1]
-print(fishes_spawn.sum())
+def calc_fishes(days, fishes):
+    for _ in range(days):
+        fishes = np.roll(fishes,-1)
+        fishes[6] += fishes[-1]
+    return fishes.sum()
+
+print(calc_fishes(80, fishes_spawn))
+print(calc_fishes(256,fishes_spawn))

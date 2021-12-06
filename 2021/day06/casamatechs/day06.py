021681cec3af7798d3867ff6507f8b74b9fb6bf0
Carlos Sanchez <casamatechs@gmail.com>
2021-12-06 21:24:20 +0900

    Removed numpy dependency

--- 2021/day06/casamatechs/day06.py
+++ 2021/day06/casamatechs/day06.py
@@ -1,20 +1,20 @@
-import numpy as np
+from functools import reduce
 
 f = open('input')
 data = [line for line in f.readlines()]
 
 fishes = [int(f) for f in data[0].split(',')]
 
-fishes_spawn = np.zeros(shape=(9,1),dtype=np.int64)
+fishes_spawn = [0] * 9
 
 for fish in fishes:
     fishes_spawn[fish] += 1
 
 def calc_fishes(days, fishes):
     for _ in range(days):
-        fishes = np.roll(fishes,-1)
+        fishes = fishes[1:] + fishes[:1]
         fishes[6] += fishes[-1]
-    return fishes.sum()
+    return reduce(lambda x,y: x + y, fishes)
 
 print(calc_fishes(80, fishes_spawn))
 print(calc_fishes(256,fishes_spawn))

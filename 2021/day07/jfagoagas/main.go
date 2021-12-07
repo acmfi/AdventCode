9e8ee47093dfb9df7dfd1f0831da2421ea76459f
Pepe Fagoaga <jose.fagoaga@smartprotection.com>
2021-12-07 11:38:45 +0100

    AoC2021 Day07 jfagoagas part1 v2

--- 2021/day07/jfagoagas/main.go
+++ 2021/day07/jfagoagas/main.go
@@ -12,9 +12,19 @@ import (
 func main() {
 	crabPositions := readLines(os.Args[1])
 	fmt.Printf("Part 1: %d\n", calcCrabFuel(crabPositions, true))
+	fmt.Printf("Part 1 v2: %d\n", calcCrabFuelv2(crabPositions))
 	fmt.Printf("Part 2: %d\n", calcCrabFuel(crabPositions, false))
-
 }
+
+func calcCrabFuelv2(crabPositions []int) int {
+	fuelSum := 0
+	sort.Ints(crabPositions)
+	for _, position := range crabPositions {
+		fuelSum += int(math.Abs(float64(position - crabPositions[len(crabPositions)/2])))
+	}
+	return fuelSum
+}
+
 func calcCrabFuel(crabPositions []int, constant bool) int {
 	var fuelCost []int
 	for _, position := range crabPositions {
@@ -25,6 +35,8 @@ func calcCrabFuel(crabPositions []int, constant bool) int {
 			if constant {
 				fuelCounter += distance
 			} else {
+				// O(1) fuelCounter += distance * (distance + 1) / 2
+				// O(n)
 				for distance > 0 {
 					fuelCounter += distance
 					distance--

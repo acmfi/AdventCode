b5f81aab16c3b1775ef62bcd432b257ab18192b5
Sergio Conde <skgsergio@gmail.com>
2021-12-09 13:02:21 +0100

    [skgsergio] 2021 Day 09: Add fancy plot

--- 2021/day09/skgsergio/main.go
+++ 2021/day09/skgsergio/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"os"
 	"sort"
 	"strconv"
@@ -117,6 +121,42 @@ func solve(points map[Point]int) (int, int) {
 	return totalRisk, basinSizes[0] * basinSizes[1] * basinSizes[2]
 }
 
+func plot(points map[Point]int, size image.Rectangle, scale int) {
+	colors := []color.RGBA{}
+	for _, c := range []string{
+		"5c53a5", "8c56a3", "b25c9c", "ce6693", "e3768a",
+		"f18a82", "f8a07e", "fab880", "f8cf8a", "f3e79b",
+	} {
+		b, err := hex.DecodeString(c)
+		panicOnErr(err)
+		colors = append(colors, color.RGBA{b[0], b[1], b[2], 255})
+	}
+
+	img := image.NewRGBA(image.Rectangle{size.Min.Mul(scale), size.Max.Mul(scale)})
+
+	for x := size.Min.X; x < size.Max.X; x++ {
+		for y := size.Min.Y; y < size.Max.Y; y++ {
+			color := color.RGBA{0, 0, 0, 0}
+			if value, ok := points[Point{x, y}]; ok {
+				color = colors[value]
+			}
+
+			for xs := 0; xs < scale; xs++ {
+				for ys := 0; ys < scale; ys++ {
+					img.Set((x*scale)+xs, (y*scale)+ys, color)
+				}
+			}
+		}
+	}
+
+	f, err := os.Create("plot.png")
+	panicOnErr(err)
+
+	png.Encode(f, img)
+
+	f.Close()
+}
+
 func main() {
 	flag.Parse()
 
@@ -135,14 +175,22 @@ func main() {
 	// Parse input
 	points := map[Point]int{}
 
+	cx := 0
+	cy := 0
 	for y, line := range strings.Split(strings.TrimSpace(string(input)), "\n") {
 		for x, value := range strings.TrimSpace(line) {
 			height, err := strconv.Atoi(string(value))
 			panicOnErr(err)
 
 			points[Point{x, y}] = height
+
+			cx = x + 1
 		}
+
+		cy = y + 1
 	}
+
+	plot(points, image.Rect(0, 0, cx, cy), 2)
 
 	// Solve
 	star1, star2 := solve(points)

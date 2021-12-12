f7d652acbdd98924e95fe6127fbdbaf9f83f29f0
Carlos Sanchez <casamatechs@gmail.com>
2021-12-12 15:44:32 +0900

    Somewhat clean code :)

--- 2021/day12/casamatechs/day12.py
+++ 2021/day12/casamatechs/day12.py
@@ -14,17 +14,7 @@ for i,o in inputs:
 
 big_cave_pattern = re.compile(r'^[A-Z]{1}')
 
-def expand_paths(input,visited,path):
-    if input == 'end':
-        complete_paths.add(path + '-end')
-        return
-    if input in caves_map.keys():
-        visited.add(input)
-        path += '-{}'.format(input) if len(path) > 0 else input
-        for out in [x for x in caves_map[input] if x not in visited or big_cave_pattern.match(x)]:
-            expand_paths(out,deepcopy(visited),path)
-
-def expand_paths_2(input,visited,path,small_visited):
+def expand_paths(input,visited,path,small_visited):
     if input == 'end':
         complete_paths.add(path + '-end')
         return
@@ -33,16 +23,16 @@ def expand_paths_2(input,visited,path,small_visited):
         path += '-{}'.format(input) if len(path) > 0 else input
         for out in [x for x in caves_map[input] if x != 'start' and (x not in visited or (x in visited and not small_visited) or big_cave_pattern.match(x))]:
             if not big_cave_pattern.match(out) and out in visited and not small_visited:
-                expand_paths_2(out,deepcopy(visited),path,True)
+                expand_paths(out,deepcopy(visited),path,True)
             else:
-                expand_paths_2(out,deepcopy(visited),path,small_visited)
+                expand_paths(out,deepcopy(visited),path,small_visited)
 
 complete_paths = set()
-expand_paths('start', set(),'')
+expand_paths('start', set(),'',True)
 
 print(len(complete_paths))
 
 complete_paths = set()
-expand_paths_2('start', set(),'',False)
+expand_paths('start', set(),'',False)
 
 print(len(complete_paths))

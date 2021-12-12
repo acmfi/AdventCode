6c741585f652deb05a6bff580579e5df9905366d
Carlos Sanchez <casamatechs@gmail.com>
2021-12-12 15:38:59 +0900

    Day 12 completed :star::star:

---
+++ 2021/day12/casamatechs/day12.py
@@ -0,0 +1,48 @@
+import re
+from copy import deepcopy
+from collections import defaultdict
+
+f = open('input')
+data = [line for line in f.readlines()]
+
+inputs = [line.strip('\n').split('-') for line in data]
+
+caves_map = defaultdict(set)
+for i,o in inputs:
+    caves_map[i].add(o)
+    caves_map[o].add(i)
+
+big_cave_pattern = re.compile(r'^[A-Z]{1}')
+
+def expand_paths(input,visited,path):
+    if input == 'end':
+        complete_paths.add(path + '-end')
+        return
+    if input in caves_map.keys():
+        visited.add(input)
+        path += '-{}'.format(input) if len(path) > 0 else input
+        for out in [x for x in caves_map[input] if x not in visited or big_cave_pattern.match(x)]:
+            expand_paths(out,deepcopy(visited),path)
+
+def expand_paths_2(input,visited,path,small_visited):
+    if input == 'end':
+        complete_paths.add(path + '-end')
+        return
+    if input in caves_map.keys():
+        visited.add(input)
+        path += '-{}'.format(input) if len(path) > 0 else input
+        for out in [x for x in caves_map[input] if x != 'start' and (x not in visited or (x in visited and not small_visited) or big_cave_pattern.match(x))]:
+            if not big_cave_pattern.match(out) and out in visited and not small_visited:
+                expand_paths_2(out,deepcopy(visited),path,True)
+            else:
+                expand_paths_2(out,deepcopy(visited),path,small_visited)
+
+complete_paths = set()
+expand_paths('start', set(),'')
+
+print(len(complete_paths))
+
+complete_paths = set()
+expand_paths_2('start', set(),'',False)
+
+print(len(complete_paths))

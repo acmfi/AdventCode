d7bacd34be9857ae46755825285a3f8ce55070f7
Carlos Sanchez <casamatechs@gmail.com>
2021-12-21 17:17:02 +0900

    Generalized solution

--- 2021/day21/casamatechs/day21.py
+++ 2021/day21/casamatechs/day21.py
@@ -39,22 +39,21 @@ print(min(p1_score,p2_score)*dice_rolled)
 p1 = int(input[0].split(' ')[-1])
 p2 = int(input[1].split(' ')[-1])
 
-movements = {}
-def check_universe(p1,p2,p1_score,p2_score):
-    if p1_score > 20:
+def check_universe(p1,p2,p1_score,p2_score,movements={},dice_size=3,winning_score=20):
+    if p1_score > winning_score:
         return (1,0)
-    if p2_score > 20:
+    if p2_score > winning_score:
         return (0,1)
     if (p1,p2,p1_score,p2_score) in movements:
         return movements[(p1,p2,p1_score,p2_score)]
     ans = (0,0)
-    for d1 in range(1,4):
-        for d2 in range(1,4):
-            for d3 in range(1,4):
+    for d1 in range(1,dice_size+1):
+        for d2 in range(1,dice_size+1):
+            for d3 in range(1,dice_size+1):
                 scr = d1+d2+d3
                 n_p1_score = p1_score + ((p1 + scr) % 10 if (p1 + scr) % 10 != 0 else 10)
                 n_p1 = (p1 + scr) % 10 if (p1 + scr) % 10 != 0 else 10
-                w1,w2 = check_universe(p2,n_p1,p2_score,n_p1_score) # Switch positions between Player 1 and 2
+                w1,w2 = check_universe(p2,n_p1,p2_score,n_p1_score,movements=movements) # Switch positions between Player 1 and 2
                 ans = (ans[0]+w2,ans[1]+w1) # Because the positions were switched, we also have to switch the tuple values when adding
     movements[(p1,p2,p1_score,p2_score)] = ans
     return ans

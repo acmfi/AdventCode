076606db30010303d062a5e530435319c08340db
Roberto Guillén <robertoguillen@me.com>
2024-12-07 00:42:01 +0100

    [2024] devcexx: day 03 part 2 - Rust

--- 2024/day03/devcexx/main.rs
+++ 2024/day03/devcexx/main.rs
@@ -9,6 +9,22 @@ mod sol1 {
             m[1].parse::<i32>().unwrap() * m[2].parse::<i32>().unwrap()
         }).sum()
     }
+
+
+    pub fn solve_muls_with_conditionals(input: &str) -> i32 {
+        let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))").unwrap();
+        re.captures_iter(input).fold((0, true), |(value, mul_enabled), m| {
+            if m.get(3).is_some() { // do
+                return (value, true);
+            } else if m.get(4).is_some() {
+                return (value, false);
+            } else if mul_enabled {
+                return (value + m[1].parse::<i32>().unwrap() * m[2].parse::<i32>().unwrap(), true);
+            } else {
+                return (value, mul_enabled)
+            }
+        }).0
+    }
 }
 
 mod sol2 {
@@ -45,13 +61,14 @@ mod sol2 {
         }).sum()
     }
 }
-
 use sol1::solve_muls;
+use sol1::solve_muls_with_conditionals;
 // use sol2::solve_muls;
 
 fn main() -> result::Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    println!("{}", solve_muls(&input));
+    println!("Multiplication result: {}", solve_muls(&input));
+    println!("Multiplication result, conditionals enabled: {}", solve_muls_with_conditionals(&input));
     Ok(())
 }
